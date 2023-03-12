97739582966585cb1eef32a950ac01855636fc38
Jirka Daněk <jirka.danek@jdanek.eu>
2023-03-12 14:58:14 +0100

    Adapt to system changes

--- plugins/extend/superprivate/SuperPrivatePlugin.php
+++ plugins/extend/superprivate/SuperPrivatePlugin.php
@@ -5,6 +5,7 @@ namespace SunlightExtend\Superprivate;
 use Sunlight\Plugin\ExtendPlugin;
 use Sunlight\Settings;
 use Sunlight\User;
+use Sunlight\WebState;
 
 /**
  * Super private pages
@@ -16,8 +17,8 @@ class SuperPrivatePlugin extends ExtendPlugin
     public function overloadTemplate(array $args): void
     {
         if (!User::isLoggedIn() && Settings::get('notpublicsite')) {
-            $args['index']->templateEnabled = false;
             $args['index']->output = require __DIR__ . '/script.php';
+            exit;
         }
     }
 }
